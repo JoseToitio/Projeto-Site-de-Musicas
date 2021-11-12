5db7118bb37e1a35c89cf2d84db19f3da59a7cc9
Jose <josetoitio@gmail.com>
2021-11-11 21:39:06 -0300

    exercicio 3 terminado

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      render: false,
+    };
+    this.getUserAPI = this.getUserAPI.bind(this);
+    this.loadUser = this.loadUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUserAPI();
+  }
+
+  getUserAPI() {
+    this.setState({
+      render: true,
+    }, async () => {
+      const getUserName = await getUser();
+      this.setState({
+        user: getUserName.name,
+        render: false,
+      });
+    });
+  }
+
+  loadUser() {
+    const { user } = this.state;
+    return (
+      <span data-testid="header-user-name">{`Usuario: ${user}`}</span>
+    );
+  }
+
+  render() {
+    const { render } = this.state;
+    return (
+      <header data-testid="header-component" className="header">
+        {render ? <Loading /> : this.loadUser()}
+        <Link to="/search">Search</Link>
+        <Link to="/album/:id">Album id</Link>
+        <Link to="/favorites">Favoritos</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/profile/edit">Profile Edit</Link>
+      </header>
+    );
+  }
+}
+
+export default Header;
